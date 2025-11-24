30b2508b8e0384de78c05432c348971614552a1c
ale12GM <garzonale3@gmail.com>
2025-11-23 21:38:33 -0400

    earreglando el progra

--- Program.cs
+++ Program.cs
@@ -53,11 +53,10 @@ using (var scope = app.Services.CreateScope())
         Console.WriteLine("Error aplicando migraciones: " + ex.Message);
     }
 }
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 // Middleware
 app.UseCors("myApp");
 app.UseAuthorization();
